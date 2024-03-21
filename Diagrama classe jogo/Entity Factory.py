0e208ab5187450213b07b9f3e26188449da76b46
Victor Emanuel <victoremanuel418@gmail.com>
2024-03-21 20:19:46 -0300

    Signed-off-by: Victor Emanuel <victoremanuel418@gmail.com>

---
+++ Diagrama classe jogo/Entity Factory.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity Factory:
+    def __init__(self):
+        pass
+
+    def get_entity(self, entity_type):
+        pass
+
---
+++ Diagrama classe jogo/Entity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.surface = None
+        self.rect = None
+
+    def move(self, ):
+        pass
+
